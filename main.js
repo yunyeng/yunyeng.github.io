dca15b7c23b0bedbc82c45e7ce7003c5eb6a6679
Yunus YEN <yunusyenigor@hotmail.com>
2015-06-20 23:32:50 -0700

    asd

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ xmlhttp.onreadystatechange = function() {
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 function toHtml(arr) {
-	var input = arr.articles;
-	var output = "";
-	for(var i = 0; i < input.length; i++) {
-		output += '<article><h3>'+input[i].title+', on <time>'+input[i].time+'</time></h3><hr><p>'+input[i].content+'</p></article>';
-	}
-	var articleDiv = document.querySelector("#articles");
-	articleDiv.innerHTML = output;
+//	var input = arr.articles;
+//	var output = "";
+//	for(var i = 0; i < input.length; i++) {
+//		output += '<article><h3>'+input[i].title+', on <time>'+input[i].time+'</time></h3><hr><p>'+input[i].content+'</p></article>';
+//	}
+//	var articleDiv = document.querySelector("#articles");
+//çarticleDiv.innerHTML = output;
 }
